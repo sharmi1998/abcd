a415b17277fdff615b26747e37d841cd917b45ac
sharmi1998 <gayu438@gmail.com>
2019-08-16 10:52:48 +0530

    my work

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Express=require('express')
 var app=new Express();
 app.set('view engine','ejs');
+app.use(Express.static(__dirname+"/public"));
 app.get('/',(req,res)=>{
     res.render('index',{title:"ictak"});
 });
